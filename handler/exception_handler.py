f751aae283772c73e5d177914c8a1c61a8f68809
Manikandan <bnairmanikandan@gmail.com>
2024-10-16 16:40:51 +0530

    add additional handler to handle NotFoundExceptions

--- handler/exception_handler.py
+++ handler/exception_handler.py
@@ -1,6 +1,5 @@
 import falcon
 
-
 class ValidationException(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -9,4 +8,13 @@ class ValidationException(Exception):
     @staticmethod
     def validation_exception_handler(ex, req, resp, params):
         resp.status = falcon.HTTP_400
+        resp.media = {'message': ex.message}
+class NotFoundException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    @staticmethod
+    def notfound_exception_handler(ex, req, resp, params):
+        resp.status = falcon.HTTP_404
         resp.media = {'message': ex.message}

bb27e2d60817ea555a5e27186ac8bb9d1771c5df
Manikandan <bnairmanikandan@gmail.com>
2024-10-17 11:50:34 +0530

    removed filtering of the  results retrieved

--- mongo_manager/mongo_manager.py
+++ mongo_manager/mongo_manager.py
@@ -1,24 +1,28 @@
 from mongo_manager.connection import DatabaseConnection
 
 class MongoManager:
-    def __init__(self, db_connection, collection_name):
-        self.db_connection = db_connection
-        self.collection = self.db_connection[collection_name]
-
-    def insert(self, user_data):
+    # def __init__(self, db_connection, collection_name):
+    #     self.db_connection = db_connection
+    #     self.collection = self.db_connection[collection_name]
+    db_connection = DatabaseConnection().get_db()
+    @classmethod
+    def insert(cls, user_data,collection_name):
         try:
-            self.collection.insert_one(user_data)
+            collection = cls.db_connection[collection_name]
+            collection.insert_one(user_data)
         except Exception:
             raise Exception("Error creating user")
 
-    def find_all(self):
-        users = self.collection.find({}, {'_id': 0})
+    @classmethod
+    def find_all(cls,collection_name):
+        collection = cls.db_connection[collection_name]
+        users = collection.find({})
         return users
 
-    def find_by_email(self, email):
-        user = self.collection.find_one({'email': email}, {'_id': 0})
+    @classmethod
+    def find_by_email(cls, email,collection_name):
+        collection = cls.db_connection[collection_name]
+        user = collection.find_one({'email': email})
         return user
 
-    def exists_by_email(self, email):
-        return self.collection.find_one({'email': email})
 

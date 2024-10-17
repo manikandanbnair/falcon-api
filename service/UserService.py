5cb1b5b96af220a4b02d414f3b339b39e2ec9d66
Manikandan <bnairmanikandan@gmail.com>
2024-10-17 11:51:30 +0530

    add filters to the results recieved

--- service/UserService.py
+++ service/UserService.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass
-
 from bson import json_util
-
 from handler.exception_handler import ValidationException, NotFoundException
-from mongo_manager.connection import DatabaseConnection
 from mongo_manager.mongo_manager import MongoManager
 
 
@@ -16,35 +13,43 @@ class User:
 
 class UserModel:
 
-    def __init__(self):
-        self.db_connection = DatabaseConnection().get_db()
-        self.mongo_manager = MongoManager(self.db_connection,collection_name="users")
-
-    def create(self, user:User):
+    _collection_name = "users"
+    @classmethod
+    def create(cls, user:User):
         user_data = user.__dict__
-        self.mongo_manager.insert(user_data)
+        MongoManager.insert(user_data,cls._collection_name)
 
-    def find_all(self):
-        users = self.mongo_manager.find_all()
+    @classmethod
+    def find_all(cls):
+        users = MongoManager.find_all(cls._collection_name)
         data = list(users)
         if not data:
             raise NotFoundException("No users present")
+
+        for user in data:
+            user.pop('_id', None)
         return json_util.dumps(data)
 
-
-    def find_by_email(self, email):
-        user = self.mongo_manager.find_by_email(email)
+    @classmethod
+    def find_by_email(cls, email):
+        user = MongoManager.find_by_email(email,cls._collection_name)
         if not user:
             raise NotFoundException("User not found")
+        user.pop('_id',None)
         return json_util.dumps(user)
 
-    def exists_by_email(self, email):
-        return self.mongo_manager.exists_by_email(email)
+    @classmethod
+    def exists_by_email(cls, email):
+        user = MongoManager.find_by_email(email,cls._collection_name)
+        if user:
+            return True
+        return False
 
-    def user_validation(self,age,email):
+    @classmethod
+    def user_validation(cls,age,email):
         if not isinstance(age,int) or age < 0:
             raise ValidationException("Invalid age")
-        if self.exists_by_email(email):
+        if cls.exists_by_email(email):
             raise ValidationException("User already exists")
 
 

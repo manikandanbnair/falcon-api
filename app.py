6937bec835ff8768f1ba9699fc49776503f2b6bf
Manikandan <bnairmanikandan@gmail.com>
2024-10-17 11:28:08 +0530

    modified logic to implement dynamic handling of errors and auto-loading

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import falcon
-from handler.exception_handler import ValidationException, NotFoundException
-from rest.UserRest import UserResource
-
-from routes.routes import register_routes
+from utils.add_error_handler import add_error_handler_function
+from utils.auto_load import auto_load
+from routes.routes import add_route
 
 app = falcon.App()
 
-register_routes(app)
-
-app.add_error_handler(ValidationException, ValidationException.validation_exception_handler)
-app.add_error_handler(NotFoundException,NotFoundException.notfound_exception_handler)
+auto_load() #load necessary modules
+add_route(app) #dynamic route creation
+add_error_handler_function(app) #dynamic error handler
